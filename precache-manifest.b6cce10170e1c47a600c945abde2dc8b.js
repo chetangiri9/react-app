3d52d13cce1784511dc43cd2f191b3317c724445
“chetangiri9” <chetangiri9@gmail.com>
2019-08-23 21:28:00 +0530

    Updates

---
+++ precache-manifest.b6cce10170e1c47a600c945abde2dc8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db3192e47462c65c4cebb270c7507e6",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "a083b2a2df3614061f9f",
+    "url": "/react-app/static/css/main.744c5823.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/react-app/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "a083b2a2df3614061f9f",
+    "url": "/react-app/static/js/main.0da6f897.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  }
+]);
